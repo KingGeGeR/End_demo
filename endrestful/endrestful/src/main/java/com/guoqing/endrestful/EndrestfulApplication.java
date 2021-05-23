c1cb6927af08bf0ca77fa0ede54b2c4bbb27f918
KingGeGeR <15605924387@163.com>
2021-05-23 16:33:29 +0800

    int spring boot restful web end

---
+++ endrestful/endrestful/src/main/java/com/guoqing/endrestful/EndrestfulApplication.java
@@ -0,0 +1,13 @@
+package com.guoqing.endrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndrestfulApplication.class, args);
+	}
+
+}
---
+++ endrestful/endrestful/src/test/java/com/guoqing/endrestful/EndrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoqing.endrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndrestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
